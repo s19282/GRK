c8171a29644002d9d029516dc0307b99f9be89a8
Mateusz <s19282@pjwstk.edu.pl>
2020-10-26 17:44:27 +0100

    Zadanie 1

--- cw1/script.js
+++ cw1/script.js
@@ -1,12 +1,19 @@
+//noprotect
 function setup()
 {
     createCanvas(1400,750);
-    background(100);
-    textFont("courier", 24);
-    text("Hello world!",20,20);
     noLoop();
 }
 function draw()
 {
-    console.log("Hello world!");
+    background(1);
+    for(y=0; y<height; y++)
+    {
+        for(x=0; x<width; x++)
+        {
+            set(x,y,color(255,0,255));
+            console.log((y));
+        }
+    }
+    updatePixels();
 }

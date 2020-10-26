fa32b0012802b096d7bb20ce1331b7c11bc0dc47
Mateusz <s19282@pjwstk.edu.pl>
2020-10-26 19:36:30 +0100

    Zadanie 4 kilka pętli

--- cw1/script.js
+++ cw1/script.js
@@ -6,15 +6,44 @@ function setup()
 }
 function draw()
 {
-    for(y=0; y<height; y++)
+    for(y=0; y<height*0.7; y++)
     {
         for(x=0; x<width; x++)
         {
-            dx=abs(x-width/2);
-            dy=abs(y-height/2);
-            d=sqrt(dx*dx+dy*dy)
-            set(x,y,color(255-d,d,(x+y)/(width+height)*256));
+            set(x,y,color(108, 250, 248));
         }
     }
+
+    for(y=height*0.7; y<height; y++)
+    {
+        for(x=0; x<width; x++)
+        {
+            set(x,y,color(35, 209, 7));
+        }
+    }
+
+    for(y=height*0.3; y<height*0.7; y++)
+    {
+        for(x=width*0.25; x<width*0.75; x++)
+        {
+            set(x,y,color(53, 19, 25));
+        }
+    }
+
+    for(y=height*0.05,a=0; y<height*0.3; y++,a+=2)
+    {
+        for(x=width/2-a; x<width/2+a; x++)
+        {
+            set(x,y,color(193, 16, 51));
+        }
+    }
+
+    for(i=0; i<1000; i++)
+    {
+        x = floor(random(0,width));
+        y = floor(random(height*0.7,height));
+        set(x,y,color("#"+floor(random()*256*256*256).toString(16)));
+    }
+
     updatePixels();
 }

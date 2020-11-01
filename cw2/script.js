1bac52bfb59c7802176345e5142bfb5f374f4ac0
Mateusz <s19282@pjwstk.edu.pl>
2020-11-01 21:23:50 +0100

    Zadanie 5

--- cw2/script.js
+++ cw2/script.js
@@ -1,68 +1,33 @@
 function preload()
 {
     img = loadImage("https://raw.githubusercontent.com/scikit-image/scikit-image/master/skimage/data/astronaut.png");
-    img_h = createImage(256,256);
-    img_s = createImage(256,256);
-    img_v = createImage(256,256);
 }
 
 function setup()
 {
-    createCanvas(512,512);
+    createCanvas(256,512);
     img.resize(256,256);
+    img.filter('gray');
     img.loadPixels();
-    img_h.loadPixels();
-    img_s.loadPixels();
-    img_v.loadPixels();
+
+    let arr = new Array(256);
+    arr.fill(0);
 
     for (x = 0; x < img.width; x++)
     {
-        for (y = 0; y < img.height; y++)
+        for(y=0; y < img.height; y++)
         {
-            pos = 4 * (y * img.width + x);
-
-            r = img.pixels[pos] / 255;
-            g = img.pixels[pos + 1] / 255;
-            b = img.pixels[pos + 2] / 255;
-
-            cmax = Math.max(r, g, b);
-            cmin = Math.min(r, g, b);
-
-            v = cmax;
-            c = cmax - cmin;
-            l=(cmax+cmin)/2;
-            s = c/(1-Math.abs(2*l-1));
-            let h;
-
-            if(c===0)
-                h=0;
-            else if(v===r)
-                h=((g-b)/c)%6;
-            else if(v===g)
-                h=((b-r)/c)+2;
-            else
-                h=((r-g)/c)+4;
-
-            h/=6;
-
-            if(h<0)
-                h+=1;
-
-            pX = (pos / 4) % 256;
-            pY = (pos / 4) / 256;
-
-            img_h.set(pX, pY, 255 * h);
-            img_s.set(pX, pY, 255 * s);
-            img_v.set(pX, pY, 255 * v);
+            pos=4*(y*img.width+x);
+            arr[img.pixels[pos]]++;
         }
     }
+    background('white');
+    stroke('gray');
 
-    img_h.updatePixels();
-    img_s.updatePixels();
-    img_v.updatePixels();
-
-    image(img_h,0,0);
-    image(img_s,256,0);
-    image(img_v,0,256);
-    image(img,256,256);
+    for(i=0; i<arr.length; i++)
+    {
+        y2 = arr[i]/Math.max(...arr)*256*10;
+        line(i,255,i,256-y2);
+    }
+    image(img,0,256);
 }

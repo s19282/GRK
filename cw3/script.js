2cca0bcafe1b28c4a872401f922e5ecfb75ff366
Mateusz <s19282@pjwstk.edu.pl>
2020-11-12 19:43:42 +0100

    Zadanie 1

--- cw3/script.js
+++ cw3/script.js
@@ -1,3 +1,8 @@
+let x0=-1;
+let y0=-1;
+let x1=-1;
+let y1=-1;
+
 function preload()
 {
 
@@ -5,5 +10,55 @@ function preload()
 
 function setup()
 {
+    createCanvas(512,512);
+    background(255);
+}
+
+function mousePressed()
+{
+    x0=mouseX;
+    y0=mouseY;
+}
+
+function mouseDragged()
+{
+    x1=mouseX;
+    y1=mouseY;
+    background(255);
+    noStroke();
+    fill('red');
+    ellipse(x0-3,y0-3,6);
+    fill('green');
+    ellipse(x1-3,y1-3,6);
+}
+
+function mouseReleased()
+{
+    background(255);
+    loadPixels();
+    draw_line();
+    updatePixels();
+}
+
+function set_pixel(x,y,c)
+{
+    idx=(y*512+x)*4;
+    pixels[idx]=c;
+    pixels[idx+1]=c;
+    pixels[idx+2]=c;
+    pixels[idx+3]=255;
+}
+
+function draw_line()
+{
+    let dx=x1-x0;
+    let dy=y1-y0;
+    let a=dy/dx;
+    let b = y0-a*x0;
+
+    for(let i=x0; i<x1; i++)
+    {
+        set_pixel(i,Math.round(a*i+b),0);
+    }
 
 }
